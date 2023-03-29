83d6793d0d5b7df7efe544501936d83416e8116e
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-03-29 19:20:44 +0200

    Reformatting and finishing level3

--- Assets/Scripts/Level03.cs
+++ Assets/Scripts/Level03.cs
@@ -16,11 +16,12 @@ public class Level03 : MonoBehaviour
         {
             case "Vampire":
                 sp.sprite = ItemManaging.instance.cape;
+                Instantiate(ItemManaging.instance.werewolf, new Vector3(135.5f, 32f, 0), Quaternion.identity);
                 break;
             case "Witch":
                 sp.sprite = ItemManaging.instance.hat;
+                Instantiate(ItemManaging.instance.ghost, new Vector3(135f, 33.7f, 0), Quaternion.identity);
                 break;
         }
-        Instantiate(ItemManaging.instance.werewolf, new Vector3(135f, 37.85f, 0), Quaternion.identity);
     }
 }
