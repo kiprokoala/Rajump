81f17974780e77997cf4834988e029e381fff188
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-03-31 23:02:39 +0200

    level 04

--- Assets/Scripts/Level03.cs
+++ Assets/Scripts/Level03.cs
@@ -7,29 +7,22 @@ public class Level03 : MonoBehaviour
     void Start()
     {
         GameObject empty = new GameObject();
-        Instantiate(empty, new Vector3(0,0,0), Quaternion.identity);
+        Instantiate(empty, new Vector3(0, 0, 0), Quaternion.identity);
         empty.AddComponent(Type.GetType("itemCollider"));
         BoxCollider2D bx = empty.AddComponent<BoxCollider2D>();
         bx.isTrigger = true;
         SpriteRenderer sp = empty.AddComponent<SpriteRenderer>();
 
-        try{
-            switch (DialogTrigger.instance.creatureName)
-            {
-                case "Vampire":
-                    sp.sprite = ItemManaging.instance.cape;
-                    Instantiate(ItemManaging.instance.werewolf, new Vector3(135.5f, 32f, 0), Quaternion.identity);
-                    break;
-                case "Witch":
-                    sp.sprite = ItemManaging.instance.hat;
-                    Instantiate(ItemManaging.instance.ghost, new Vector3(135f, 33.7f, 0), Quaternion.identity);
-                    break;
-            }
-        }
-        catch //This is just for tests
+        switch (DialogTrigger.instance.creatureName)
         {
-            sp.sprite = ItemManaging.instance.cape;
-            Instantiate(ItemManaging.instance.werewolf, new Vector3(135.5f, 32f, 0), Quaternion.identity);
+            case "Vampire":
+                sp.sprite = ItemManaging.instance.cape;
+                Instantiate(ItemManaging.instance.werewolf, new Vector3(135.5f, 32f, 0), Quaternion.identity);
+                break;
+            case "Witch":
+                sp.sprite = ItemManaging.instance.hat;
+                Instantiate(ItemManaging.instance.ghost, new Vector3(135f, 33.7f, 0), Quaternion.identity);
+                break;
         }
     }
 }
--- Assets/Scripts/Level04.cs
+++ Assets/Scripts/Level04.cs
@@ -13,15 +13,17 @@ public class Level04 : MonoBehaviour
             {
                 case "Vampire":
                     Instantiate(ItemManaging.instance.werewolf, new Vector3(28f, -22f, 0), Quaternion.identity);
+                    Instantiate(ItemManaging.instance.werewolf, new Vector3(51f, 0, 0), Quaternion.identity);
                     break;
                 case "Witch":
                     Instantiate(ItemManaging.instance.ghost, new Vector3(24f, -20f, 0), Quaternion.identity);
+                    Instantiate(ItemManaging.instance.ghost, new Vector3(47f, 2f, 0), Quaternion.identity);
                     break;
             }
         }
         catch //This is just for tests
         {
-            Instantiate(ItemManaging.instance.ghost, new Vector3(27f, -18f, 0), Quaternion.identity);
+
         }
     }
 }
