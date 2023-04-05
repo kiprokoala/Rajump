55ac12f4a333a02da35b730ea54289d5949f0ca8
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-04-05 08:28:43 +0200

    Witch side 4

--- Assets/Scripts/ItemManaging.cs
+++ Assets/Scripts/ItemManaging.cs
@@ -50,14 +50,14 @@ public class ItemManaging : MonoBehaviour
         catch //This is just for tests
         {
 
-            position.y += 0.35f;
-            sp.sprite = cape;
-            PlayerMovement.instance.moveSpeed *= 1.5f;
+            //position.y += 0.35f;
+            //sp.sprite = cape;
+            //PlayerMovement.instance.moveSpeed *= 1.5f;
 
-            //position.y += 0.05f;
-            //position.x += 0.25f;
-            //sp.sprite = hat;
-            //PlayerMovement.instance.jumpForce *= 2;
+            position.y += 0.05f;
+            position.x += 0.25f;
+            sp.sprite = hat;
+            PlayerMovement.instance.jumpForce *= 2;
         }
 
         empty.transform.position = position;
