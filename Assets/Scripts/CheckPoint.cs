43b8ca5d9a8ab385d459feb01c93c67d4fe07205
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-04-17 10:00:32 +0200

    visibilité des checkpoints + player at 0;0

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
+    public Sprite checkpoint_checked;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            transform.GetComponent<SpriteRenderer>().sprite = checkpoint_checked;
             CurrentSceneManager.instance.respawnPoint = transform.position;
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
--- Assets/Scripts/CurrentSceneManager.cs
+++ Assets/Scripts/CurrentSceneManager.cs
@@ -19,30 +19,18 @@ public class CurrentSceneManager : MonoBehaviour
         respawnPoint = GameObject.FindGameObjectWithTag("Player").transform.position;
     }
 
-    private void Start()
-    {
-        PlayerMovement.instance.transform.position = new Vector3(PlayerPrefs.GetFloat("posx"), PlayerPrefs.GetFloat("posy"), 0);
-        PlayerPrefs.SetFloat("posx", -5f);
-        PlayerPrefs.SetFloat("posy", 0);
-        coinsPickedUpInCurrentScene = PlayerPrefs.GetInt("coinsCollected");
-        removeCoinsCollected();
-    }
-
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-                Inventory.instance.removeCoins(instance.coinsPickedUpInCurrentScene);
+            Inventory.instance.removeCoins(instance.coinsPickedUpInCurrentScene);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             PlayerHealth.instance.currentHealth = PlayerPrefs.GetInt("life");
             PlayerHealth.instance.lifebar.setHealth(PlayerHealth.instance.currentHealth);
         }
         if (Input.GetKeyDown(KeyCode.C))
         {
-            PlayerPrefs.SetFloat("posx", instance.respawnPoint.x);
-            PlayerPrefs.SetFloat("posy", instance.respawnPoint.y);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            PlayerPrefs.SetInt("coins", Inventory.instance.coinsCount);
+            PlayerMovement.instance.transform.position = respawnPoint;
         }
     }
 }
